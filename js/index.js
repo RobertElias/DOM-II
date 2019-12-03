0bbc873b86bba0159a9ad8c3ea7331009e3bfd34
Robert <Robert.Elias@live.com>
2019-12-03 12:13:58 -0800

    .nav mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+//Mouse Over on .nav class
+
+const nav = document.querySelector('nav');
+nav.addEventListener("mouseover", ()=>{
+    nav.style.transition = "2.5s";
+    nav.style.backgroundColor = "papawhip"; 
+})
